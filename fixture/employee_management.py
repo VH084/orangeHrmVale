d485b60bc8493588de9604f4007f1931cf4dff0d
VH084 <valeshka26@gmail.com>
2024-04-17 20:06:30 +0200

    tc 9 table

--- fixture/employee_management.py
+++ fixture/employee_management.py
@@ -2,9 +2,13 @@
 from fixture.step import StepHelper
 from selenium.webdriver.remote.webdriver import WebDriver
 
+from fixture.table import Table
+
+
 class EmployeeManagement:
     home_button = "a[data-automation-id='menu_home']"
     list_of_widgets_header_texts = ".widget-header span:last-child"
+    employee_first_table_row = '#employeeListTable tbody tr'
 
     def __init__(self, step: StepHelper, wd: WebDriver):
         self.step = step
@@ -15,3 +19,16 @@ class EmployeeManagement:
 
     def get_widgets_headers(self):
         return self.step.get_elements_texts(self.list_of_widgets_header_texts)
+
+    def __init__(self, step: StepHelper, wd: WebDriver):
+        self.step = step
+        self.wd = wd
+        self.table = Table(step,
+                           row_selector='#employeeListTable tbody tr',
+                           column_selectors={'employee_id': 'td:nth-child(2)',
+                                             'employee_name': 'td:nth-child(3)',
+                                             'employee_job_title': 'td:nth-child(4)',
+                                             'employment_status': 'td:nth-child(5)'})
+
+    def wait_for_table(self):
+        self.step.wait_for_element(self.employee_first_table_row, 40)
--- test/test_case_9_employee_management_table_verification.py
+++ test/test_case_9_employee_management_table_verification.py
@@ -3,9 +3,20 @@ def test_case_9_employee_management_table_verification(app):
     app.orangeHrm.login_to_the_application()
     app.assert_that(app.orangeHrm.get_header_text()).is_equal_to('Employee Management')
     app.orangeHrm.sideMenu.click_on_side_menu_button("Employee Management")
+    app.orangeHrm.employeeManagement.wait_for_table()
+    a = app.orangeHrm.employeeManagement.table.get_column_data('employee_id')
+    b = app.orangeHrm.employeeManagement.table.get_column_data('employee_name')
+    c = app.orangeHrm.employeeManagement.table.get_column_data('employee_job_title')
+    d = app.orangeHrm.employeeManagement.table.get_column_data('employment_status')
+    app.assert_that(app.orangeHrm.employeeManagement.table[1]['employee_id']).is_equal_to('1061')
+    app.assert_that(app.orangeHrm.employeeManagement.table[0]['employee_name']).is_equal_to('Mazie Abraham')
+    app.assert_that(app.orangeHrm.employeeManagement.table[4]['employment_status']).is_equal_to('Full-Time Permanent')
 
-    # 1 Create a new object of the table class inside the employee management component (based on the example from hr_administration).
-    # 2 Find selectors: For list of rows and list of column elements (Employee Id, Name, Job Title, Employment Status).
+    print(a, b, c, d)
+
+
+    ## 1 Create a new object of the table class inside the employee management component (based on the example from hr_administration).
+    ## 2 Find selectors: For list of rows and list of column elements (Employee Id, Name, Job Title, Employment Status).
     # 3 Get list of elements from each defined column and assert them with the expected one.
     # 4 Get second element from the 'Employee Id' column and assert it with the expected one.
     # 5 Get first element from the 'Name' column and assert it with the expected one.
